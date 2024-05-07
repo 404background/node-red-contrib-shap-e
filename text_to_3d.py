2b6aedc04cebc278acb59bd42d7a0c21940f8900
404background <404background@gmail.com>
2024-05-07 18:50:26 +0900

    add Python programs

---
+++ text_to_3d.py
@@ -0,0 +1,32 @@
+import torch
+from IPython.display import display
+
+from shap_e.models.download import load_model
+from shap_e.util.data_util import load_or_create_multimodal_batch
+from shap_e.util.notebooks import create_pan_cameras, decode_latent_images, gif_widget
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+xm = load_model('transmitter', device=device)
+
+model_path = "./object.obj"
+
+# This may take a few minutes, since it requires rendering the model twice
+# in two different modes.
+batch = load_or_create_multimodal_batch(
+    device,
+    model_path=model_path,
+    mv_light_mode="basic",
+    mv_image_size=256,
+    cache_dir="./cache",
+    verbose=True, # this will show Blender output during renders
+)
+
+with torch.no_grad():
+    latent = xm.encoder.encode_to_bottleneck(batch)
+
+    render_mode = 'stf' # you can change this to 'nerf'
+    size = 128 # recommended that you lower resolution when using nerf
+
+    cameras = create_pan_cameras(size, device)
+    images = decode_latent_images(xm, latent, cameras, rendering_mode=render_mode)
+    display(gif_widget(images))
